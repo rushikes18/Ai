c6984a5360d1a37d2b828ceb1b1bf4b71172db42
RUSHIKESH <153503460+rushikes18@users.noreply.github.com>
2026-01-04 15:27:38 +0530

    Remove unnecessary newline in auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,7 +2,6 @@ import express from "express"
 import { Login, logOut, signUp } from "../controllers/auth.controllers.js"
 
 const authRouter=express.Router()
-
 authRouter.post("/signup",signUp)
 authRouter.post("/signin",Login)
 authRouter.get("/logout",logOut)
